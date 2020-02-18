b3fee43a100dbab5dc0fbcbfcdef81f2b0d0af8f
deepmal933 <deepmal944@gmail.com>
2020-02-18 00:30:57 -0500

    added emoji picker

--- src/routes/Write.jsx
+++ src/routes/Write.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Trix from "trix";
 import "../styles/trix.css";
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 
 const Write = props => {
   return (
     <div className="write flx-hor-center">
       <div className="card mgn-20" style={{ display: "block" ,width:800+"px"}}>
         <div className="card-content pad-15">
+        <Picker set='emojione' />
+
           <form>
             <input
               id="x"
